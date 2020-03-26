63921025fa218b726646e41c33757849b98a9633
FelipeMelon <62666016+FelipeMelon@users.noreply.github.com>
2020-03-26 11:12:07 +0000

    Update yarpen copy.py

--- yarpen copy.py
+++ yarpen copy.py
@@ -4,8 +4,6 @@ import random
 import string
 
 
-
-
 client = discord.Client()
 
 @client.event
@@ -21,29 +19,34 @@ async def on_message(message):
     if message.content.startswith('Elo'):
         await message.channel.send('Witam wielmożnego pana!')
 
+    elif message.content.startswith('Witam'):
+        await message.channel.send('Witaj! Zjedzmy coś i się napijmy!')            
 
-    elif message.content.startswith('Spamrundie'):
-        await message.channel.send('He is so gay that you cannot believe it!')
+    elif message.content.startswith('elo'):
+        await message.channel.send('Witam wielmożnego pana!')
 
-    elif message.content.startswith('Zoltan jest lepszy od yarpena'):
+    elif message.content.startswith('.Zoltan jest lepszy od yarpena'):
         await message.channel.send('Odrąbię Ci ten głupi łeb toporem!')
 
-    elif message.content.startswith('Skąd macie mięsko?'):
+    elif message.content.startswith('.Skąd macie mięsko?'):
         await message.channel.send('Samo przypełzło!')
 
-    elif message.content.startswith('Napijmy się!'):
+    elif message.content.startswith('.Napijmy się!'):
         await message.channel.send(':beers:')
 
-    elif message.content.startswith('Bitka!'):
+    elif message.content.startswith('.pieszgitarą'):
+        await message.channel.send(file=discord.File('Pies.png'))
+
+    elif message.content.startswith('.Bitka!'):
         await message.channel.send('Bitka? Gdzie?!')
         await message.channel.send(file=discord.File('Krasnolud.png'))
 
-    elif message.content.startswith('krzak'):
+    elif message.content.startswith('.krzak'):
         await message.channel.send('Proszę bardzo:')
 
-        a = ['krzak1.png', 'krzak2.png', 'krzak3.png', 'krzak4.png', 'krzak5.png', 'krzak6.png', 'krzak7.png', 'krzak8.png', 'krzak9.png', 'krzak10.png', 'krzak12.png', 'krzak13.png', 'krzak14.png', 'krzak15.png' ]
+        listaObrazkow = ['krzak1.png', 'krzak2.png', 'krzak3.png', 'krzak4.png', 'krzak5.png', 'krzak6.png', 'krzak7.png', 'krzak8.png', 'krzak9.png', 'krzak10.png', 'krzak11.png', 'krzak12.png', 'krzak13.png', 'krzak14.png', 'krzak15.png' ]
 
-        await message.channel.send(file=discord.File(random.choice(a)))
+        await message.channel.send(file=discord.File(random.choice(listaObrazkow)))
 
 
 
